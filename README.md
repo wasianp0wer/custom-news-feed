5ab6061fafedf019f953be0f3b118e7011128a2d
maddie <madelyn.j.harrington@gmail.com>
2025-01-15 16:38:47 -0500

    readme + caching

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Cloned from https://github.com/eaiti/eai-sveltekit-starter.
+
+Are we live? [![Netlify Status](https://api.netlify.com/api/v1/badges/62d7d28c-a2dc-4241-9c7d-243c7c6a9b1d/deploy-status)](https://app.netlify.com/sites/tubular-trifle-764899/deploys)
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,26 +1,54 @@
 import { fail } from '@sveltejs/kit';
 import type { PageServerLoad, Actions } from './$types';
 import { XMLParser, XMLBuilder, XMLValidator } from 'fast-xml-parser';
-import { RssParser } from '../util/rss-parser';
+import { RssParser, type RssPage } from '../util/rss-parser';
 
-type GuardianFeed = {
-	media: string;
-};
+const cache = new Map<string, RssPage>();
+let lastCacheUpdate: Date | undefined = undefined;
 
 export const load = (async ({ cookies }) => {
-	const parser = new RssParser();
-	const newsFeed = await parser.parseUrl('https://www.theguardian.com/us/rss');
-	const fiftyFirstFeed = await parser.parseUrl('https://51st.news/rss/');
-	const guardianOpinionFeed = await parser.parseUrl('https://www.theguardian.com/us/commentisfree/rss');
-	const propublicaFeed = await parser.parseUrl('https://theintercept.com/feed/');
-	const varietyFeed = await parser.parseUrl('https://variety.com/feed/');
+	if (!lastCacheUpdate || lastCacheUpdate.getTime() + 60000 < Date.now()) {
+		lastCacheUpdate = new Date();
+		cache.clear();
+		const parser = new RssParser();
+		await Promise.all([
+			parser.parseUrl('https://www.theguardian.com/us/rss').then((rss) => {
+				cache.set(CacheSource.NEWS, rss);
+			}),
+			parser.parseUrl('https://51st.news/rss/').then((rss) => {
+				cache.set(CacheSource.LOCAL, rss);
+			}),
+			parser.parseUrl('https://www.theguardian.com/us/commentisfree/rss').then((rss) => {
+				cache.set(CacheSource.OPINION, rss);
+			}),
+			parser.parseUrl('https://theintercept.com/feed/').then((rss) => {
+				cache.set(CacheSource.INVESTIGATIVE, rss);
+			}),
+			parser.parseUrl('https://variety.com/feed/').then((rss) => {
+				cache.set(CacheSource.POP_CULTURE, rss);
+			})
+		]);
+	}
+	const newsFeed = cache.get(CacheSource.NEWS);
+	const fiftyFirstFeed = cache.get(CacheSource.LOCAL);
+	const guardianOpinionFeed = cache.get(CacheSource.OPINION);
+	const propublicaFeed = cache.get(CacheSource.INVESTIGATIVE);
+	const varietyFeed = cache.get(CacheSource.POP_CULTURE);
 	return {
-		newsItems: newsFeed.items,
-		localItems: fiftyFirstFeed.items,
-		opinionItems: guardianOpinionFeed.items,
-		investigativeItems: propublicaFeed.items,
-		popCultureItems: varietyFeed.items
+		newsItems: newsFeed?.items ?? [],
+		localItems: fiftyFirstFeed?.items ?? [],
+		opinionItems: guardianOpinionFeed?.items ?? [],
+		investigativeItems: propublicaFeed?.items ?? [],
+		popCultureItems: varietyFeed?.items ?? []
 	};
 }) satisfies PageServerLoad;
 
 export const actions = {} satisfies Actions;
+
+export enum CacheSource {
+	NEWS = 'news',
+	LOCAL = 'local',
+	OPINION = 'opinion',
+	INVESTIGATIVE = 'investigative',
+	POP_CULTURE = 'popCulture'
+}
